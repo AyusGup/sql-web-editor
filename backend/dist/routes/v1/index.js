59db6f9561d27a824b479c30bb3db58db1d50414
Ayush Gupta <127961367+AyusGup@users.noreply.github.com>
2026-02-26 00:47:44 +0530

    Delete backend/dist directory

--- backend/dist/routes/v1/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-// import assignmentRoutes from "./assignment.routes";
-const router = (0, express_1.Router)();
-// router.use("/assignment", assignmentRoutes);
-exports.default = router;
